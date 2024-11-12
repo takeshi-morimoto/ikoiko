d643cf40e8aaea5a388eccf5e89ee4e44480e2d6
takeshi-morimoto <takeshi.morimoto@koikoi.co.jp>
2024-11-12 23:22:10 +0900

    footerの画像URLを変更。マッチングアプリメディアを削除

--- widgets/footer.php
+++ widgets/footer.php
@@ -1,59 +1,45 @@
 <div id="footer">
-
-
-	<div class="footer-banner">
-		
-		<ul>
-			
-		<li class="machikon">
-			<a href="//machicon.jp/" target="_blank">
-					<img src="/ikoiko/img/banners/japan.jpg" alt="全国街コン公式サイト 街コンジャパン" title="街コンジャパン">
-				</a>
-		</li>
-
-		<li class="matome">
-			<a href="//machicom-matome.com" target="_blank">
-				<img src="/ikoiko/img/banners/matome.jpg" alt="東京神奈川千葉埼玉大阪名古屋福岡の情報なら街コンまとめへ">
-			</a>
-		</li>
-
-		<li class="omikare">
-			<a href="//party-calendar.net" target="_blank">
-				<img src="/ikoiko/img/banners/omikare.jpg" alt="オミカレ">
-			</a>
-		</li>
-
-
-		<li class="portal">
-			<a href="//www.machicom.jp/" target="_blank">
-				<img src="/ikoiko/img/banners/portal.jpg" alt="街コンポータル">
-			</a>
-		</li>
-		</ul>
-
-		<ul>
-
-		<li class="matching">
-			<a href="https://jsbs2012.jp/" target="_blank" rel="noopener">
-				<img src="/ikoiko/img/banners/matching_university.jpg" alt="マッチングアプリ大学"></a>
-		</li>
-
-		<li class="e-venz">
-			<a href="https://e-venz.com/" target="_blank" rel="noopener">
-				<img src="/ikoiko/img/banners/e-venz.jpg" alt="イベンツ">
-			</a>
-		</li>
-
-		<li class="marriage_hunting_paradise">
-			<a href="https://lifrell.co.jp/marriage/" target="_blank" rel="noopener">
-				<img src="/ikoiko/img/banners/konkatsu-paradise-br.png" alt="婚活パラダイス">
-			</a>
-		</li>
-
-
-
-		</ul>
-	</div>
+    <div class="footer-banner">
+        <ul>
+            <li class="machikon">
+                <a href="//machicon.jp/" target="_blank">
+                    <img src="/ikoiko/img/common/footer_banners/japan.jpg" alt="全国街コン公式サイト 街コンジャパン" title="街コンジャパン">
+                </a>
+            </li>
+            <li class="matome">
+                <a href="//machicom-matome.com" target="_blank">
+                    <img src="/ikoiko/img/common/footer_banners/matome.jpg" alt="東京神奈川千葉埼玉大阪名古屋福岡の情報なら街コンまとめへ">
+                </a>
+            </li>
+            <li class="omikare">
+                <a href="//party-calendar.net" target="_blank">
+                    <img src="/ikoiko/img/common/footer_banners/omikare.jpg" alt="オミカレ">
+                </a>
+            </li>
+            <li class="portal">
+                <a href="//www.machicom.jp/" target="_blank">
+                    <img src="/ikoiko/img/common/footer_banners/portal.jpg" alt="街コンポータル">
+                </a>
+            </li>
+        </ul>
+        <ul>
+            <li class="matching">
+                <a href="https://jsbs2012.jp/" target="_blank" rel="noopener">
+                    <img src="/ikoiko/img/common/footer_banners/matching_university.jpg" alt="マッチングアプリ大学">
+                </a>
+            </li>
+            <li class="e-venz">
+                <a href="https://e-venz.com/" target="_blank" rel="noopener">
+                    <img src="/ikoiko/img/common/footer_banners/e-venz.jpg" alt="イベンツ">
+                </a>
+            </li>
+            <li class="marriage_hunting_paradise">
+                <a href="https://lifrell.co.jp/marriage/" target="_blank" rel="noopener">
+                    <img src="/ikoiko/img/common/footer_banners/konkatsu-paradise-br.png" alt="婚活パラダイス">
+                </a>
+            </li>
+        </ul>
+    </div>
 
 	<style>
 
@@ -119,7 +105,6 @@
 		<ul>
 			<li><a target="blank" href="//koikoi.co.jp/">運営会社　｜　</a></li>
 			<li><a href="//koikoi.co.jp/ikoiko/ポリシー.php">プライバシーポリシー　｜　</a></li>
-			<li><a href="//koikoi.co.jp/media/">マッチングアプリメディア　｜　</a></li>
 			<li><a href="//koikoi.co.jp/ikoiko/特商法.php">特定商取引に基づく表記　｜　</a></li>
 			<li><a href="//koikoi.co.jp/ikoiko/参加ルール.php">参加ルール</a></li>
 		</ul>

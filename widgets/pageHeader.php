750a53aefee9f390970d92da938209bf633fd275
Takeshi Morimoto <takeshi.morimoto@koikoi.co.jp>
2024-11-12 18:01:55 +0900

    Refactor: Consolidate image assets and optimize directory structure
    - Moved and renamed icon and common images to `img/common/icon/`
    - Deleted redundant images in `img/comp/` and optimized layout
    - Updated `pageHeader.php` to reflect new image paths

--- widgets/pageHeader.php
+++ widgets/pageHeader.php
@@ -1,23 +1,35 @@
-<a class="mb_none" ><img class="mb_none" id="logo" src="//koikoi.co.jp/ikoiko/img/logo.png" alt="" /></a>
+<a class="mb_none">
+    <picture>
+        <source srcset="//koikoi.co.jp/ikoiko/img/common/logo.webp" type="image/webp">
+        <img class="mb_none" id="logo" src="//koikoi.co.jp/ikoiko/img/common/logo.png" alt="ロゴ">
+    </picture>
+</a>
 
 <div id="contact">
-	
-		<div class="box_001"><span>コンビニ・カード決済もOK！</span><img class="icon_card" src="//koikoi.co.jp/ikoiko/img/icon/icon_card.png" alt="" /></div>
+    <div class="box_001">
+        <span>コンビニ・カード決済もOK！</span>
+        <picture>
+            <source srcset="//koikoi.co.jp/ikoiko/img/common/icons/icon_card.webp" type="image/webp">
+            <img class="icon_card" src="//koikoi.co.jp/ikoiko/img/common/icons/icon_card.png" alt="カード決済アイコン">
+        </picture>
+    </div>
 
-		<div class="box_002 mb_none">
-			<img class="icon_tel" src="//koikoi.co.jp/ikoiko/img/icon/icon_tel.png" alt="" />
-			<span>03-6754-6371</span>
-		</div>
-
+    <div class="box_002 mb_none">
+        <picture>
+            <source srcset="//koikoi.co.jp/ikoiko/img/common/icons/icon_tel.webp" type="image/webp">
+            <img class="icon_tel" src="//koikoi.co.jp/ikoiko/img/common/icons/icon_tel.png" alt="電話アイコン">
+        </picture>
+        <span>03-6754-6371</span>
+    </div>
 </div>
 
-<script type="text/javascript">jQuery(function() {  
+<script>
+jQuery(function() {  
     jQuery("a").click(function(e) {        
         var ahref = jQuery(this).attr('href');
-        if (ahref.indexOf("//koikoi.co.jp/ikoiko/") != -1 || ahref.indexOf("http") == -1 ) {
-            ga('send', 'event', '内部リンク', 'クリック', ahref);} 
-        else { 
-            ga('send', 'event', '外部リンク', 'クリック', ahref);}
-        });
+        var eventType = ahref.includes("//koikoi.co.jp/ikoiko/") || ahref.indexOf("http") === -1 
+                        ? '内部リンク' : '外部リンク';
+        ga('send', 'event', eventType, 'クリック', ahref);
     });
+});
 </script>

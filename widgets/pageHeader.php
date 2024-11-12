e59019e87bcc57de5d43d33b37ec51623fc8ee42
takeshi-morimoto <takeshi.morimoto@koikoi.co.jp>
2024-11-12 22:51:25 +0900

    ヘッダー修正

--- widgets/pageHeader.php
+++ widgets/pageHeader.php
@@ -1,24 +1,15 @@
 <a class="mb_none">
-    <picture>
-        <source srcset="//koikoi.co.jp/ikoiko/img/common/logo.webp" type="image/webp">
-        <img class="mb_none" id="logo" src="//koikoi.co.jp/ikoiko/img/common/logo.png" alt="ロゴ">
-    </picture>
+    <img class="mb_none" id="logo" src="//koikoi.co.jp/ikoiko/img/common/logo.png" alt="ロゴ">
 </a>
 
 <div id="contact">
     <div class="box_001">
         <span>コンビニ・カード決済もOK！</span>
-        <picture>
-            <source srcset="//koikoi.co.jp/ikoiko/img/common/icons/icon_card.webp" type="image/webp">
-            <img class="icon_card" src="//koikoi.co.jp/ikoiko/img/common/icons/icon_card.png" alt="カード決済アイコン">
-        </picture>
+        <img class="icon_card" src="//koikoi.co.jp/ikoiko/img/common/icons/icon_card.png" alt="カード決済アイコン">
     </div>
 
     <div class="box_002 mb_none">
-        <picture>
-            <source srcset="//koikoi.co.jp/ikoiko/img/common/icons/icon_tel.webp" type="image/webp">
-            <img class="icon_tel" src="//koikoi.co.jp/ikoiko/img/common/icons/icon_tel.png" alt="電話アイコン">
-        </picture>
+        <img class="icon_tel" src="//koikoi.co.jp/ikoiko/img/common/icons/icon_tel.png" alt="電話アイコン">
         <span>03-6754-6371</span>
     </div>
 </div>

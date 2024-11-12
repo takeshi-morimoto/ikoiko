5248d072cd2cc64e685d652760226ef3f7e586ef
takeshi-morimoto <takeshi.morimoto@koikoi.co.jp>
2024-11-12 23:43:25 +0900

    icon関連のディレクトリを変更。謎解きとオフ会のページを削除

--- widgets/ani/dateSearch.php
+++ widgets/ani/dateSearch.php
@@ -1,243 +1,161 @@
-
 <div class="dateSearch">
-
 	<script src="//koikoi.co.jp/ikoiko/js/dateSearch.js"></script>
 
 	<h2>カレンダーから検索</h2>
 
-	<div id="next"><img src="//koikoi.co.jp/ikoiko/img/icon/icon_next.png" alt="" /></div>
-	<div id="prev"><img src="//koikoi.co.jp/ikoiko/img/icon/icon_prev.png" alt="" /></div>
+	<div id="next"><img src="//koikoi.co.jp/ikoiko/img/common/icon/icon_next.png" alt="" /></div>
+	<div id="prev"><img src="//koikoi.co.jp/ikoiko/img/common/icon/icon_prev.png" alt="" /></div>
 
 	<div id="dateSearch_inner">
 
-<?php 
+	<?php 
+	if ( isset($searchArea) ):
+	  $area = $searchArea . '/';
+	else: 
+	  $area = '';
+	endif;
 
-if ( isset($searchArea) ):
+	if ( isset($searchDate) ):
+	  $date = $searchDate . '/';
+	else: 
+	  $date = '';
+	endif;
 
-  $area = $searchArea. '/';
+	$y = date('Y');
+	$m = date('n');
+	$d = 1;
+	?>
 
-else: 
+	<div id="calendar_this" class="cal">
+		<div>
+			<h3><?php print "{$y}年{$m}月"; ?></h3>
+		</div>
 
-  $area = '';
+		<table>
+			<tr><td><span class="txtc_red">日</span></td><td>月</td><td>火</td><td>水</td><td>木</td><td>金</td><td><span class="txtc_blue">土</span></td></tr>
+			<tr>
+			<?php
+				// 最初の週の空白を調整
+				$fw = date( 'w', mktime(0,0,0,$m,1,$y) );
 
-endif;
+				for ( $n = 1 ; $n <= $fw ; $n++ ):
+					print '<td> </td>';
+				endfor;
 
+				while ( checkdate($m, $d, $y) ):
+					if ( date('w', mktime(0,0,0,$m,$d,$y) ) == 0 ):
+						print '</tr><tr>';
+					endif;
 
-if ( isset($searchDate) ):
+					$thisDate = "{$y}-{$m}-{$d}";
+					print "<td class='" . CSR( 'ani', $searchArea, $thisDate ) . "'><a href='//koikoi.co.jp/ikoiko/list_1/{$thisDate}/'>{$d}</a></td>";
+					$d++;
+				endwhile;
 
-  $date = $searchDate. '/';
+				// 最後の週の空白を調整
+				$d--;
+				$lw = date( 'w', mktime(0,0,0,$m,$d,$y) );
 
-else: 
-
-  $date = '';
-
-endif;
-
-
-$y = date('Y');
-$m = date('n');
-$d = 1;
-
-?>
-
-<div id="calendar_this" class="cal">
-
-	<div>
-		<h3><?php print "{$y}年{$m}月"; ?></h3>
+				while ( $lw < 6 ):
+					print '<td> </td>';
+					$lw++;
+				endwhile;
+			?>
+			</tr>
+		</table>
 	</div>
 
-	<table>
-		
-		<tr><td><span class="txtc_red">日</span></td><td>月</td><td>火</td><td>水</td><td>木</td><td>金</td><td><span class="txtc_blue">土</span></td></tr>
-		<tr>
+	<?php 
+	// 翌月の設定
+	if ( $m == 12 ):	
+		$y++;
+		$m = 1;
+	else:				
+		$m++;
+	endif;
 
-		<?php
+	$d = 1;
+	?>
 
-			// 最初の週の空白を調整
-			$fw = date( 'w', mktime(0,0,0,$m,1,$y) );
+	<div id="calendar_next" class="cal">
+		<div>
+			<h3><?php print "{$y}年{$m}月"; ?></h3>
+		</div>
 
-			for ( $n = 1 ; $n <= $fw ; $n++ ):
+		<table>
+			<tr><td><span class="txtc_red">日</span></td><td>月</td><td>火</td><td>水</td><td>木</td><td>金</td><td><span class="txtc_blue">土</span></td></tr>
+			<tr>
+			<?php
+				$fw = date( 'w', mktime(0,0,0,$m,1,$y) );
+				for ( $n = 1 ; $n <= $fw ; $n++ ):
+					print '<td> </td>';
+				endfor;
 
-				print '<td> </td>';
+				while ( checkdate($m, $d, $y) ):
+					if ( date('w', mktime(0,0,0,$m,$d,$y) ) == 0 ):
+						print '</tr><tr>';
+					endif;
 
-			endfor;
+					$thisDate = "{$y}-{$m}-{$d}";
+					print "<td class='" . CSR( 'ani', $searchArea, $thisDate ) . "'><a href='//koikoi.co.jp/ikoiko/list_1/{$thisDate}/'>{$d}</a></td>";
+					$d++;
+				endwhile;
 
-			while ( checkdate($m, $d, $y) ):
-
-				if ( date('w', mktime(0,0,0,$m,$d,$y) ) == 0 ):
-
-					print '</tr><tr>';
-
-				endif;
-
-				$thisDate = "{$y}-{$m}-{$d}";
-				print "<td class='" . CSR( 'ani', $searchArea, $thisDate ) . "'><a href='//koikoi.co.jp/ikoiko/list_1/{$thisDate}/'>{$d}</a></td>";
-
-				$d++;
-
-			endwhile;
-
-			// 最後の週の空白を調整
-			$d--;
-			$lw = date( 'w', mktime(0,0,0,$m,$d,$y) );
-
-			while ( $lw < 6 ):
-
-				print '<td> </td>';
-				$lw++;
-
-			endwhile;
-
-		?>
-
-		</tr>
-	</table>
-</div>
-
-<?php 
-
-if ( $m == 12 ):	// 12月の場合は年と月を設定
-
-	$y++;
-	$m = 1;
-
-else:				// それ以外の場合は翌月を設定
-
-	$m++;
-
-endif;
-
-$d = 1;
-
-?>
-
-<div id="calendar_next" class="cal">
-
-	<div>
-		<h3><?php print "{$y}年{$m}月"; ?></h3>
+				$d--;
+				$lw = date( 'w', mktime(0,0,0,$m,$d,$y) );
+				while ( $lw < 6 ):
+					print '<td> </td>';
+					$lw++;
+				endwhile;
+			?>
+			</tr>
+		</table>
 	</div>
 
-	<table>
-		
-		<tr><td><span class="txtc_red">日</span></td><td>月</td><td>火</td><td>水</td><td>木</td><td>金</td><td><span class="txtc_blue">土</span></td></tr>
-		<tr>
+	<?php 
+	if ( $m == 12 ):	
+		$y++;
+		$m = 1;
+	else:				
+		$m++;
+	endif;
 
-		<?php
+	$d = 1;
+	?>
 
-			// 最初の週の空白を調整
-			$fw = date( 'w', mktime(0,0,0,$m,1,$y) );
+	<div id="calendar_next2" class="cal">
+		<div>
+			<h3><?php print "{$y}年{$m}月"; ?></h3>
+		</div>
 
-			for ( $n = 1 ; $n <= $fw ; $n++ ):
+		<table>
+			<tr><td><span class="txtc_red">日</span></td><td>月</td><td>火</td><td>水</td><td>木</td><td>金</td><td><span class="txtc_blue">土</span></td></tr>
+			<tr>
+			<?php
+				$fw = date( 'w', mktime(0,0,0,$m,1,$y) );
+				for ( $n = 1 ; $n <= $fw ; $n++ ):
+					print '<td> </td>';
+				endfor;
 
-				print '<td> </td>';
+				while ( checkdate($m, $d, $y) ):
+					if ( date('w', mktime(0,0,0,$m,$d,$y) ) == 0 ):
+						print '</tr><tr>';
+					endif;
 
-			endfor;
+					$thisDate = "{$y}-{$m}-{$d}";
+					print "<td class='" . CSR( 'ani', $searchArea, $thisDate ) . "'><a href='//koikoi.co.jp/ikoiko/list_1/{$thisDate}/'>{$d}</a></td>";
+					$d++;
+				endwhile;
 
-			while ( checkdate($m, $d, $y) ):
-
-				if ( date('w', mktime(0,0,0,$m,$d,$y) ) == 0 ):
-
-					print '</tr><tr>';
-
-				endif;
-
-				$thisDate = "{$y}-{$m}-{$d}";
-
-				print "<td class='" . CSR( 'ani', $searchArea, $thisDate ) . "'><a href='//koikoi.co.jp/ikoiko/list_1/{$thisDate}/'>{$d}</a></td>";
-
-				$d++;
-
-			endwhile;
-
-			// 最後の週の空白を調整
-			$d--;
-			$lw = date( 'w', mktime(0,0,0,$m,$d,$y) );
-
-			while ( $lw < 6 ):
-
-				print '<td> </td>';
-				$lw++;
-
-			endwhile;
-
-		?>
-
-		</tr>
-	</table>
-</div>
-
-
-<?php 
-
-if ( $m == 12 ):	// 12月の場合は年と月を設定
-
-	$y++;
-	$m = 1;
-
-else:				// それ以外の場合は翌月を設定
-
-	$m++;
-
-endif;
-
-$d = 1;
-
-?>
-
-<div id="calendar_next2" class="cal">
-
-	<div>
-		<h3><?php print "{$y}年{$m}月"; ?></h3>
+				$d--;
+				$lw = date( 'w', mktime(0,0,0,$m,$d,$y) );
+				while ( $lw < 6 ):
+					print '<td> </td>';
+					$lw++;
+				endwhile;
+			?>
+			</tr>
+		</table>
 	</div>
-
-	<table>
-		
-		<tr><td><span class="txtc_red">日</span></td><td>月</td><td>火</td><td>水</td><td>木</td><td>金</td><td><span class="txtc_blue">土</span></td></tr>
-		<tr>
-
-		<?php
-
-			// 最初の週の空白を調整
-			$fw = date( 'w', mktime(0,0,0,$m,1,$y) );
-
-			for ( $n = 1 ; $n <= $fw ; $n++ ):
-
-				print '<td> </td>';
-
-			endfor;
-
-			while ( checkdate($m, $d, $y) ):
-
-				if ( date('w', mktime(0,0,0,$m,$d,$y) ) == 0 ):
-
-					print '</tr><tr>';
-
-				endif;
-
-				$thisDate = "{$y}-{$m}-{$d}";
-
-				print "<td class='" . CSR( 'ani', $searchArea, $thisDate ) . "'><a href='//koikoi.co.jp/ikoiko/list_1/{$thisDate}/'>{$d}</a></td>";
-
-				$d++;
-
-			endwhile;
-
-			// 最後の週の空白を調整
-			$d--;
-			$lw = date( 'w', mktime(0,0,0,$m,$d,$y) );
-
-			while ( $lw < 6 ):
-
-				print '<td> </td>';
-				$lw++;
-
-			endwhile;
-
-		?>
-
-		</tr>
-	</table>
-</div>
-
 </div><!-- /dateSearch_inner -->
 </div><!-- /dateSearch -->
--- widgets/machi/dateSearch.php
+++ widgets/machi/dateSearch.php
@@ -1,38 +1,27 @@
-
 <div class="dateSearch">
 
 	<script src="//koikoi.co.jp/ikoiko/js/dateSearch.js"></script>
 
 	<h2>カレンダーから検索</h2>
 
-	<div id="next"><img src="//koikoi.co.jp/ikoiko/img/icon/icon_next.png" alt="" /></div>
-	<div id="prev"><img src="//koikoi.co.jp/ikoiko/img/icon/icon_prev.png" alt="" /></div>
+	<div id="next"><img src="//koikoi.co.jp/ikoiko/img/common/icon/icon_next.png" alt="" /></div>
+	<div id="prev"><img src="//koikoi.co.jp/ikoiko/img/common/icon/icon_prev.png" alt="" /></div>
 
 	<div id="dateSearch_inner">
 
 <?php 
 
 if ( isset($searchArea) ):
-
   $area = $searchArea. '/';
-
 else: 
-
   $area = '';
-
 endif;
-
 
 if ( isset($searchDate) ):
-
   $date = $searchDate. '/';
-
 else: 
-
   $date = '';
-
 endif;
-
 
 $y = date('Y');
 $m = date('n');
@@ -47,49 +36,31 @@ $d = 1;
 	</div>
 
 	<table>
-		
 		<tr><td><span class="txtc_red">日</span></td><td>月</td><td>火</td><td>水</td><td>木</td><td>金</td><td><span class="txtc_blue">土</span></td></tr>
 		<tr>
 
 		<?php
-
-			// 最初の週の空白を調整
 			$fw = date( 'w', mktime(0,0,0,$m,1,$y) );
-
 			for ( $n = 1 ; $n <= $fw ; $n++ ):
-
 				print '<td> </td>';
-
 			endfor;
 
 			while ( checkdate($m, $d, $y) ):
-
 				if ( date('w', mktime(0,0,0,$m,$d,$y) ) == 0 ):
-
 					print '</tr><tr>';
-
 				endif;
-
 
 				$thisDate = "{$y}-{$m}-{$d}";
 				print "<td class='" . CSR( 'machi', $searchArea, $thisDate ) . "'><a href='//koikoi.co.jp/ikoiko/list_2/{$thisDate}/'>{$d}</a></td>";
-
-
 				$d++;
-
 			endwhile;
 
-			// 最後の週の空白を調整
 			$d--;
 			$lw = date( 'w', mktime(0,0,0,$m,$d,$y) );
-
 			while ( $lw < 6 ):
-
 				print '<td> </td>';
 				$lw++;
-
 			endwhile;
-
 		?>
 
 		</tr>
@@ -97,20 +68,14 @@ $d = 1;
 </div>
 
 <?php 
-
-if ( $m == 12 ):	// 12月の場合は年と月を設定
-
+if ( $m == 12 ):
 	$y++;
 	$m = 1;
-
-else:				// それ以外の場合は翌月を設定
-
+else:				
 	$m++;
-
 endif;
 
 $d = 1;
-
 ?>
 
 <div id="calendar_next" class="cal">
@@ -120,70 +85,46 @@ $d = 1;
 	</div>
 
 	<table>
-		
 		<tr><td>日</td><td>月</td><td>火</td><td>水</td><td>木</td><td>金</td><td>土</td></tr>
 		<tr>
 
 		<?php
-
-			// 最初の週の空白を調整
 			$fw = date( 'w', mktime(0,0,0,$m,1,$y) );
-
 			for ( $n = 1 ; $n <= $fw ; $n++ ):
-
 				print '<td> </td>';
-
 			endfor;
 
 			while ( checkdate($m, $d, $y) ):
-
 				if ( date('w', mktime(0,0,0,$m,$d,$y) ) == 0 ):
-
 					print '</tr><tr>';
-
 				endif;
 
 				$thisDate = "{$y}-{$m}-{$d}";
 				print "<td class='" . CSR( 'machi', $searchArea, $thisDate ) . "'><a href='//koikoi.co.jp/ikoiko/list_2/{$thisDate}/'>{$d}</a></td>";
-
-
 				$d++;
-
 			endwhile;
 
-			// 最後の週の空白を調整
 			$d--;
 			$lw = date( 'w', mktime(0,0,0,$m,$d,$y) );
-
 			while ( $lw < 6 ):
-
 				print '<td> </td>';
 				$lw++;
-
 			endwhile;
-
 		?>
 
 		</tr>
 	</table>
 </div>
 
-
 <?php 
-
-if ( $m == 12 ):	// 12月の場合は年と月を設定
-
+if ( $m == 12 ):
 	$y++;
 	$m = 1;
-
-else:				// それ以外の場合は翌月を設定
-
+else:				
 	$m++;
-
 endif;
 
 $d = 1;
-
 ?>
 
 <div id="calendar_next2" class="cal">
@@ -193,48 +134,31 @@ $d = 1;
 	</div>
 
 	<table>
-		
 		<tr><td>日</td><td>月</td><td>火</td><td>水</td><td>木</td><td>金</td><td>土</td></tr>
 		<tr>
 
 		<?php
-
-			// 最初の週の空白を調整
 			$fw = date( 'w', mktime(0,0,0,$m,1,$y) );
-
 			for ( $n = 1 ; $n <= $fw ; $n++ ):
-
 				print '<td> </td>';
-
 			endfor;
 
 			while ( checkdate($m, $d, $y) ):
-
 				if ( date('w', mktime(0,0,0,$m,$d,$y) ) == 0 ):
-
 					print '</tr><tr>';
-
 				endif;
 
 				$thisDate = "{$y}-{$m}-{$d}";
 				print "<td class='" . CSR( 'machi', $searchArea, $thisDate ) . "'><a href='//koikoi.co.jp/ikoiko/list_2/{$thisDate}/'>{$d}</a></td>";
-
-
 				$d++;
-
 			endwhile;
 
-			// 最後の週の空白を調整
 			$d--;
 			$lw = date( 'w', mktime(0,0,0,$m,$d,$y) );
-
 			while ( $lw < 6 ):
-
 				print '<td> </td>';
 				$lw++;
-
 			endwhile;
-
 		?>
 
 		</tr>

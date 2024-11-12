f87f23992799a20a1776e092d0a7aa3e1ffe3f76
takeshi-morimoto <takeshi.morimoto@koikoi.co.jp>
2024-11-12 22:56:57 +0900

    ヘッダー修正

--- widgets/pageHeader.php
+++ widgets/pageHeader.php
@@ -1,26 +1,28 @@
-<a class="mb_none" href="//koikoi.co.jp/ikoiko/"> <!-- href属性を追加 -->
-    <img class="mb_none" id="logo" src="//koikoi.co.jp/ikoiko/img/common/logo.png" alt="ロゴ">
-</a>
+<div id="pageHeader">
+    <a class="mb_none" href="//koikoi.co.jp/ikoiko/">
+        <img class="mb_none" id="logo" src="//koikoi.co.jp/ikoiko/img/common/logo.png" alt="ロゴ">
+    </a>
 
-<div id="contact">
-    <div class="box_001">
-        <span>コンビニ・カード決済もOK！</span>
-        <img class="icon_card" src="//koikoi.co.jp/ikoiko/img/common/icons/icon_card.png" alt="カード決済アイコン">
+    <div id="contact">
+        <div class="box_001">
+            <span>コンビニ・カード決済もOK！</span>
+            <img class="icon_card" src="//koikoi.co.jp/ikoiko/img/common/icons/icon_card.png" alt="カード決済アイコン">
+        </div>
+
+        <div class="box_002 mb_none">
+            <img class="icon_tel" src="//koikoi.co.jp/ikoiko/img/common/icons/icon_tel.png" alt="電話アイコン">
+            <span>03-6754-6371</span>
+        </div>
     </div>
 
-    <div class="box_002 mb_none">
-        <img class="icon_tel" src="//koikoi.co.jp/ikoiko/img/common/icons/icon_tel.png" alt="電話アイコン">
-        <span>03-6754-6371</span>
-    </div>
-</div>
-
-<script>
-jQuery(function() {  
-    jQuery("a").click(function(e) {        
-        var ahref = jQuery(this).attr('href');
-        var eventType = ahref.includes("//koikoi.co.jp/ikoiko/") || ahref.indexOf("http") === -1 
-                        ? '内部リンク' : '外部リンク';
-        ga('send', 'event', eventType, 'クリック', ahref);
+    <script>
+    jQuery(function() {  
+        jQuery("a").click(function(e) {        
+            var ahref = jQuery(this).attr('href');
+            var eventType = ahref.includes("//koikoi.co.jp/ikoiko/") || ahref.indexOf("http") === -1 
+                            ? '内部リンク' : '外部リンク';
+            ga('send', 'event', eventType, 'クリック', ahref);
+        });
     });
-});
-</script>
+    </script>
+</div>

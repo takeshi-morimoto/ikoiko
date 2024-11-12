8efedb1157c666487416a1e92a3aa66ff922e9ca
takeshi-morimoto <takeshi.morimoto@koikoi.co.jp>
2024-11-12 22:40:02 +0900

    もとに戻す

--- yoruuuu.php
+++ yoruuuu.php
@@ -16,58 +16,147 @@
 <div id="topContainer">
 	
 	<div id="pageHeader">
+		
 		<?php include("/home/users/1/lolipop.jp-30251d4519441da4/web/ikoiko/widgets/pageHeader.php") ?>
+
 	</div>
 	
 	<div id="mainVisual">
+					
 		<?php include("/home/users/1/lolipop.jp-30251d4519441da4/web/ikoiko/widgets/ani/globalMenu.php") ?>
 		<?php include("/home/users/1/lolipop.jp-30251d4519441da4/web/ikoiko/widgets/ani/globalMenu_mb.php") ?>
+
 	</div>
 
 	<div id="mainContainer">
 		<div id="mainContent">
-			<!-------------------------------- メインコンテンツ[Start] -------------------------------->
+		<!-------------------------------- メインコンテンツ[Start] -------------------------------->
 
-			<div class="event_freeArea">
-				<div class="event_freeArea_inner">
-					<div class="banner" style="padding: 0 20px 10px;">
-						<a href="/ikoiko/yoruuuu/">
-							<img src="/ikoiko/img/common/services/yoruuuuutop.jpg" alt="yoruuuutop" title="yoruuuutop" width="100%">
-						</a>
-					</div>
+<div class="event_freeArea">
+<div class="event_freeArea_inner">		
+<p>
+		<div class="banner" style="padding: 0 20px 10px;">
+		<a href="/ikoiko/yoruuuu/">
+			<img src="/ikoiko/img/banners/yoruuuuutop.jpg" alt="yoruuuutop" title="yoruuuutop" width="100%">
+		</a>
+		</div>
 
-					<h4>※大変申し訳ございません。感染状況、不要不急の外出抑制を受けて、一旦募集を停止させていただきます</h4>
+<br>
+<h4><font>※大変申し訳ございません。感染状況、不要不急の外出抑制を受けて、一旦募集を停止させていただきます</font></h4>
+<br>
 
-					<h4 style="color: red;">一緒にこっそり、イメージチェンジしてみませんか？</h4>
+<br>
 
-					<p>オタクスタッフがお買い物に同行し、ファッションの提案をするサービスをはじめました。</p>
+		<h4><font color = "red">一緒にこっそり、イメージチェンジしてみませんか？</font></h4>
 
-					<h4 style="color: red;">【あか抜けたい/脱オタしたい ・・・でもどうすればいいの？】</h4>
-					<p>ファッションについて女性目線で寄り添ってスタイリング。まずあなたの理想のスタイルをお聞かせください！（もちろん、お任せでもOK！）</p>
+<br>
+<br>
+		オタクスタッフがお買い物に同行し、ファッションの提案をするサービスをはじめました。
+<br>
+<br>
+		コロナ禍で人と会う機会が減ってしまいましたね…<br>
+		そんな今こそ、新しい自分にチャレンジしてみませんか！！<br>
+<br>
+		あなたの“なりたい”に近づくお手伝いします！<br>
+<br>
+		<h4><font color = "red">【あか抜けたい/脱オタしたい<br>
+		　　　　　・・・でもどうすればいいの？】</font></h4>
+		<br>
+<br>
+		ファッションについて女性目線で寄り添ってスタイリング。<br>
+		まずあなたの理想のスタイルをお聞かせください！<br>
+		（もちろん、お任せでもOK！）<br>
+<br>
+		あなたのなりたいスタイルやシーン、体系に合ったファッションを<br>
+		相談しながら提案いたします。<br>
+		あなたの勝負モテファッション、一緒に探しましょう！<br>
+<br>
+<br>
 
-					<h4 style="color: red;">【たとえば・・・ めいっぱいカワイイ！に身を包みたい ・・・でもどうすればいいの？】</h4>
-					<p>kawaii文化のファッションブランドの知識に強みがあります。一緒にたのしく新しいジャンルのお洋服に挑戦してみませんか？</p>
+		<h4><font color = "red">【たとえば・・・<br>めいっぱいカワイイ！に身を包みたい<br>
+		　　　　　・・・でもどうすればいいの？】</font></h4>
+<br>
+		kawaii文化のファッションブランドの知識に強みがあります。<br>
+		一緒にたのしく新しいジャンルのお洋服に挑戦してみませんか？<br>
+		”100％僕もかわいくなりたいナ！”という<br>
+		女装に挑戦してみたい方も大歓迎です！カワイイは正義！！<br>
+<br>
+		◎お洋服に合ったトータルコーディネートのご相談も可能です！！<br>
+		　ご希望の場合は同行します。男性女性どちらでも可です！<br>
+<br>
+		・ヘアカット/カラー<br>
+		・ヘアセット<br>
+		・眉カット<br>
+		・メイク<br>
+		・ネイル<br>
+		・(デートコース相談）<br>
+<br>
+		ご希望がありましたら、<br>
+		ＳＮＳ用写真の撮影もお手伝いします！<br>
+		※ご自身のスマホでの撮影です。<br>
+<br>
+		<h4>価格</h4>
+<br>
+			現在テスト期間中につき、1万円～のところ、<font color = "red">先着3名申し込み無料です！</font><br>
+			洋服等は実費での購入となります。もちろん何も購入しなくてもOKです。<br>
+<br>
 
-					<h4>価格</h4>
-					<p>現在テスト期間中につき、1万円～のところ、<span style="color: red;">先着3名申し込み無料です！</span></p>
+		<h4>当日の流れ</h4>
+<br>
+		１．打ち合わせのご都合の良い日程問い合わせフォームよりご連絡ください。<br>
+		2.お電話やZOOMなどで日時、場所、要望などの詳細をお聞きします。<br>
+		3.<s>お支払いについてご連絡致しますので指定の口座にお振込みをお願いします。</s><font color = "red">現在無料期間につきお支払いは必要ございません。</font><br>
+		4.指定した場所にてお買い物やファッションのアドバイスを致します。<br>
 
-					<h4>当日の流れ</h4>
-					<p>１．打ち合わせのご都合の良い日程問い合わせフォームよりご連絡ください。</p>
+<br>
+		<h4>プロフィール</h4>
 
-					<h4>プロフィール</h4>
-					<p>ＹＯＲＵ（よる） 東京の街コンＭＣをしております。</p>
+<br>
+		ＹＯＲＵ（よる）<br>
+<br>
+		　東京の街コンＭＣをしております。<br>
+		　イベント前のご依頼大歓迎！！！<br>
+		　一番魅力的なあなたでイベントに挑みましょう！<br>
+<br>
+		▼略歴<br>
+		　都内のファッション学校 デザイン科を卒業（服飾専門士）<br>
+		　都内大学へ編入、マネジメントを学ぶ。<br>
+		　大学卒業後、上場企業入社、同時に株式会社ＫＯＩＫＯＩでＭＣを担当<br>
+<br>
+		◆活動範囲<br>
+<br>
+		　東京都　千代田区/豊島区/新宿区/渋谷区（範囲外の場合もお気軽にご相談ください。）<br>
+<br>
+		<h4>特記事項</h4><br>
+<br>
+		コロナウイルス感染防止のため<br>
+		コーディネーターはマスクを着用しています。<br>
+		飲食を伴うデートコース相談は現在募集を停止しています。<br>
+<br>
+		コーディネーターは一緒に悩み、寄り添いますが<br>
+		チェンジ内容に物品購入や施術が伴う場合、<br>
+		決定は依頼主本人が行います。<br>
+<br>
+		同意なく無理な購入や施術を勧めるようなことは、<br>
+		一切ございません。
+<br>
+<br>
 
-					<h4>特記事項</h4>
-					<p>コロナウイルス感染防止のため、コーディネーターはマスクを着用しています。</p>
-
-					<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdo0eL6_m0ZRY_CkNb1AjXWeDrcnOUGyU5wWMALiGigNolcwg/viewform?embedded=true" width="640" height="1540" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
-				</div>
-			</div>
-			<!-------------------------------- メインコンテンツ[End] -------------------------------->
+		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdo0eL6_m0ZRY_CkNb1AjXWeDrcnOUGyU5wWMALiGigNolcwg/viewform?embedded=true" width="640" height="1540" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+<br>
+<br>
+<br>
+<br>
+</p>
+</div>
+</div>
+		<!-------------------------------- メインコンテンツ[End] -------------------------------->
 		</div>
 
 		<div id="sideContent">
+			
 			<?php include("/home/users/1/lolipop.jp-30251d4519441da4/web/ikoiko/widgets/sideContent.php") ?>
+
 		</div>
 	</div>
 

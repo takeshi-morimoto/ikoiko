d0d8b23909485d3cf36402f9a84a6043f8d882fc
Takeshi Morimoto <takeshi.morimoto@koikoi.co.jp>
2024-11-12 18:44:18 +0900

    マッチングアプリメディアを削除

--- widgets/footer.php
+++ widgets/footer.php
@@ -6,6 +6,7 @@
       <li><a href="//party-calendar.net" target="_blank"><img src="/ikoiko/img/common/footer_banners/omikare.jpg" alt="オミカレ"></a></li>
       <li><a href="//www.machicom.jp/" target="_blank"><img src="/ikoiko/img/common/footer_banners/portal.jpg" alt="街コンポータル"></a></li>
     </ul>
+
     <ul>
       <li><a href="https://jsbs2012.jp/" target="_blank" rel="noopener"><img src="/ikoiko/img/common/footer_banners/matching_university.jpg" alt="マッチングアプリ大学"></a></li>
       <li><a href="https://e-venz.com/" target="_blank" rel="noopener"><img src="/ikoiko/img/common/footer_banners/e-venz.jpg" alt="イベンツ"></a></li>
@@ -17,7 +18,6 @@
     <ul class="footer-menu">
       <li><a href="//koikoi.co.jp/" target="blank">運営会社</a></li>
       <li><a href="//koikoi.co.jp/ikoiko/ポリシー.php">プライバシーポリシー</a></li>
-      <li><a href="//koikoi.co.jp/media/">マッチングアプリメディア</a></li>
       <li><a href="//koikoi.co.jp/ikoiko/特商法.php">特定商取引に基づく表記</a></li>
       <li><a href="//koikoi.co.jp/ikoiko/参加ルール.php">参加ルール</a></li>
     </ul>

0f6aec504f2b52719c8e3f742657553c5af94943
takeshi-morimoto <takeshi.morimoto@koikoi.co.jp>
2024-11-12 23:14:49 +0900

    マッチングも変更

--- widgets/pageHeader_m.php
+++ widgets/pageHeader_m.php
@@ -1,23 +1,26 @@
-<a class="mb_none"><img class="mb_none" id="logo" src="//koikoi.co.jp/ikoiko/img/logo.png" alt="" /></a>
+<a class="mb_none" href="//koikoi.co.jp/ikoiko/"><img class="mb_none" id="logo" src="//koikoi.co.jp/ikoiko/img/common/icon/logo.png" alt="ロゴ" /></a>
 
 <div id="contact">
-	
-		<div class="box_001"><span>コンビニ・カード決済もOK！</span><img class="icon_card" src="//koikoi.co.jp/ikoiko/img/icon/icon_card.png" alt="" /></div>
+    <div class="box_001">
+        <span>コンビニ・カード決済もOK！</span>
+        <img class="icon_card" src="//koikoi.co.jp/ikoiko/img/common/icon/icon_card.png" alt="カード決済アイコン" />
+    </div>
 
-		<div class="box_002 mb_none">
-			<img class="icon_tel" src="//koikoi.co.jp/ikoiko/img/icon/icon_tel.png" alt="" />
-			<span>03-6754-6371</span>
-		</div>
-
+    <div class="box_002 mb_none">
+        <img class="icon_tel" src="//koikoi.co.jp/ikoiko/img/common/icon/icon_tel.png" alt="電話アイコン" />
+        <span>03-6754-6371</span>
+    </div>
 </div>
 
-<script type="text/javascript">jQuery(function() {  
+<script type="text/javascript">
+jQuery(function() {  
     jQuery("a").click(function(e) {        
         var ahref = jQuery(this).attr('href');
-        if (ahref.indexOf("//koikoi.co.jp/ikoiko/") != -1 || ahref.indexOf("http") == -1 ) {
-            ga('send', 'event', '内部リンク', 'クリック', ahref);} 
-        else { 
-            ga('send', 'event', '外部リンク', 'クリック', ahref);}
-        });
+        if (ahref.indexOf("//koikoi.co.jp/ikoiko/") != -1 || ahref.indexOf("http") == -1) {
+            ga('send', 'event', '内部リンク', 'クリック', ahref);
+        } else { 
+            ga('send', 'event', '外部リンク', 'クリック', ahref);
+        }
     });
+});
 </script>

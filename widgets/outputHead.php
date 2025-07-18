07b82f627c6bca18e5984e6e92ae3227d8312fa2
takeshi-morimoto <takeshi.morimoto@koikoi.co.jp>
2025-07-18 10:22:33 +0900

    wsi

--- widgets/outputHead.php
+++ widgets/outputHead.php
@@ -2,7 +2,10 @@
 <meta name="format-detection" content="telephone=no">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-<link rel="stylesheet" href="https://koikoi.co.jp/ikoiko/css/all.min.css" />
+<link rel="stylesheet" href="https://koikoi.co.jp/ikoiko/css/base.css" />
+<link rel="stylesheet" href="https://koikoi.co.jp/ikoiko/css/content.css" />
+<link rel="stylesheet" href="https://koikoi.co.jp/ikoiko/css/globalMenu.css" />
+<link rel="stylesheet" href="https://koikoi.co.jp/ikoiko/css/onepcssgrid.css" />
 <link rel="icon" href="https://koikoi.co.jp/ikoiko/img/favicon.ico" />
 
 <script type='text/javascript' src='https://koikoi.co.jp/ikoiko/js/jquery-3.1.0.min.js'></script>
